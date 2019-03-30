5a920e0da1671421725ac0ff3a0e4d0f608c2c65
Michalina Krysiak <gabi.krysiak@gmail.com>
2019-03-30 17:46:04 +0100

    cofigured environment

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+
+    ]
+  }
+
+};
